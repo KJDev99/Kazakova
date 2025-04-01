1beb5ef4055c168167b67feef7762f18d8f9a21b
Jamshid <jamshidqayimov0399@gmail.com>
2025-04-01 13:02:42 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="text-3xl font-bold">Старт</h1>
+      <p className="mt-4 text-lg">Спасибо, что работаете со мной</p>
+    </div>
+  );
+}
