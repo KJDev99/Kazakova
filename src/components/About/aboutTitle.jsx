f81c62461578a62fa9f8c2661422771cd6597e11
Jamshid <jamshidqayimov0399@gmail.com>
2025-04-04 10:07:06 +0500

    about add page

---
+++ src/components/About/aboutTitle.jsx
@@ -0,0 +1,9 @@
+export default function AboutTitle() {
+  return (
+    <div>
+      <div className="px-22 flex flex-col">
+        <h2 className="text-[80px] mb-18">О нас</h2>
+      </div>
+    </div>
+  );
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import AboutNav from "../components/About/aboutNav";
+import AboutTitle from "../components/About/aboutTitle";
+import AboutContent from "../components/About/aboutContent";
+import Forma from "../components/forma";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <AboutNav />
+      <AboutTitle />
+      <AboutContent />
+      <Forma />
+    </>
+  );
 }
